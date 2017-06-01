f3db9e8ddc31a14785b772593c3a020e1f51b598
Georgiy Odisharia <math.kraut.cat@gmail.com>
2017-06-01 23:55:04 +0300

    ioctl() now almost works

--- spi-protocol-generic.c
+++ spi-protocol-generic.c
@@ -27,7 +27,8 @@
 
 #ifdef DEBUG_SPI_PROTOCOL_GENERIC
 #define debug_printk(...) printk(KERN_DEBUG "spi-protocol-generic: "); \
-	printk(KERN_CONT __VA_ARGS__)
+		printk(KERN_CONT __VA_ARGS__); \
+		printk(KERN_CONT "\n")
 #else /* DEBUG_SPI_PROTOCOL_GENERIC */
 #define debug_printk(...) do {} while(0)
 #endif /* DEBUG_SPI_PROTOCOL_GENERIC */
@@ -51,19 +52,11 @@ static dev_t first_dev_t;
 static unsigned minorcount;
 static struct class		*spi_protocol_generic_class;
 
-struct spi_device_default_values {
-	u32 speed_hz;
-	u16 mode;
-	u8  bits_per_word;
-};
-
 struct meta_information {
 	struct spi_device *device;
-	struct spi_device_default_values default_values;
 
 	dev_t device_maj_min;
-	struct cdev *char_device;
-	struct device *sysfs_device;
+	struct cdev *internal_cdev;
 
 	u8 *tx_buffer_reg;
 	u8 *rx_buffer_reg;
@@ -74,8 +67,8 @@ struct meta_information {
 static LIST_HEAD(meta_info_list);
 static DEFINE_MUTEX(device_list_lock);
 
-static const unsigned buf_reg_size = 2;
-static const unsigned n_reg_xfers = 2;
+static const unsigned buf_reg_size = 4;
+static const unsigned n_reg_xfers = 4;
 
 #ifdef MY_BUGGY_DT_OVERLAY
 enum {
@@ -101,11 +94,15 @@ static const struct arduino_board_info arduino_info[] = {
 static const struct of_device_id spi_protocol_generic_of_match[] = {
 	{
 		.compatible = "arduino,pro-mini-spi-generic",
+#ifdef MY_BUGGY_DT_OVERLAY
 		.data = &arduino_info[pro_mini],
+#endif /* MY_BUGGY_DT_OVERLAY */
 	},
 	{
 		.compatible = "arduino,due-spi-generic",
+#ifdef MY_BUGGY_DT_OVERLAY
 		.data = &arduino_info[due],
+#endif /* MY_BUGGY_DT_OVERLAY */
 	},
 	{ },
 };
@@ -133,18 +130,21 @@ __make_reg_message(struct spi_transfer *k_xfer)
 }
 
 static void
-__make_req_reg(struct spi_transfer *xfer)
+__make_req_reg(struct spi_transfer **xfer_pp)
 {
+	struct spi_transfer *xfer = *xfer_pp;
 	xfer->tx_buf = &request_registry;
 	xfer->rx_buf = NULL;
-	xfer->delay_usecs = 1;
+	//xfer->delay_usecs = 1000;
+	(*xfer_pp)++;
 }
 
 static void
-__make_req_cmd(struct spi_transfer *xfer, u8 cmd)
+__make_req_cmd(struct spi_transfer **xfer_pp, u8 cmd)
 {
+	struct spi_transfer *xfer = *xfer_pp;
 	xfer->rx_buf = NULL;
-	xfer->delay_usecs = 1;
+	//xfer->delay_usecs = 1000;
 	switch (cmd) {
 	case SET_REGISTER:
 		xfer->tx_buf = &set_register;
@@ -153,20 +153,15 @@ __make_req_cmd(struct spi_transfer *xfer, u8 cmd)
 		xfer->tx_buf = &get_register;
 		break;
 	}
+	(*xfer_pp)++;
 }
 
 static struct spi_transfer *
-__make_reg_transfers(struct register_info *reg_info)
+__make_set_reg_transfers(struct meta_information *meta_info,
+			 struct register_info *reg_info)
 {
 	struct spi_transfer *xfer, *head;
-	u8 *tx_buf;
-	unsigned n;
-
-	// Copy to global TX buffer values of reg_info fields
-	*tx_buffer_reg = reg_info->reg_addr;
-	*(tx_buffer_reg + 1) = reg_info->value;
-
-	tx_buf = tx_buffer_reg;
+	int i;
 
 	xfer = kcalloc(4, sizeof(struct spi_transfer), GFP_KERNEL);
 	if (xfer == NULL)
@@ -174,24 +169,30 @@ __make_reg_transfers(struct register_info *reg_info)
 
 	head = xfer;
 
-	__make_req_reg(xfer++);
-	__make_req_cmd(xfer++, SET_REGISTER);
+	__make_req_reg(&xfer);
+	__make_req_cmd(&xfer, SET_REGISTER);
 
-	for (n = n_reg_xfers;
-	     n;
-	     n--, xfer++, tx_buf++) {
-		xfer->tx_buf = tx_buf;
-		xfer->rx_buf = NULL;
-		xfer->delay_usecs = 1;
-	}
+	for (i = 0; i < 4; i++)
+		(head + i)->len = 1;
+
+	xfer->tx_buf = &(reg_info->reg_addr);
+	xfer->rx_buf = NULL;
+	//xfer->delay_usecs = 1000;
+
+	xfer++;
+
+	xfer->tx_buf = &(reg_info->value);
+	xfer->rx_buf = NULL;
 
 	return head;
 }
 
 static struct spi_transfer *
-__make_get_reg_transfers(struct register_info *reg_info)
+__make_get_reg_transfers(struct meta_information *meta_info,
+			 struct register_info *reg_info)
 {
 	struct spi_transfer *xfer, *head;
+	int i;
 
 	xfer = kcalloc(4, sizeof(struct spi_transfer), GFP_KERNEL);
 	if (xfer == NULL)
@@ -199,12 +200,15 @@ __make_get_reg_transfers(struct register_info *reg_info)
 
 	head = xfer;
 
-	__make_req_reg(xfer++);
-	__make_req_cmd(xfer++, GET_REGISTER);
+	for (i = 0; i < 4; i++)
+		(head + i)->len = 1;
+
+	__make_req_reg(&xfer);
+	__make_req_cmd(&xfer, GET_REGISTER);
 
 	xfer->tx_buf = &(reg_info->reg_addr);
 	xfer->rx_buf = NULL;
-	xfer->delay_usecs = 1;
+	//xfer->delay_usecs = 1000;
 
 	xfer++;
 
@@ -215,16 +219,34 @@ __make_get_reg_transfers(struct register_info *reg_info)
 }
 
 // Char subsystem functions;
-static int spi_protocol_generic_open(struct inode *inode, struct file *file_p)
+static int
+spi_protocol_generic_open(struct inode *inode, struct file *file_p)
 {
 	int retval;
+        int status = -ENXIO;
+	struct meta_information *meta_info;
+
+	list_for_each_entry(meta_info, &meta_info_list, list_entry) {
+		if (meta_info->device_maj_min == inode->i_rdev) {
+			status = 0;
+			break;
+		}
+	}
+
+	if (status) {
+		return status;
+	}
+
+	file_p->private_data = meta_info;
+
 	retval = nonseekable_open(inode, file_p);
 	return retval;
 }
 
-static int spi_protocol_generic_release(struct inode *inode,
-					struct file *file_p)
+static int
+spi_protocol_generic_release(struct inode *inode, struct file *file_p)
 {
+	file_p->private_data = NULL;
 	return 0;
 }
 
@@ -233,10 +255,12 @@ spi_protocol_generic_read(struct file *file_p, char __user *buf, size_t lbuf,
 		loff_t *ppos)
 {
 	int err;
+	struct meta_information *meta_info = file_p->private_data;
+	struct spi_device *__spi_device_internal = meta_info->device;
 	u8 *read_buffer = kcalloc(2, sizeof(u8), GFP_KERNEL);
 	struct spi_transfer read_arduino[1] = {};
 
-	printk(KERN_DEBUG "spi-protocol-generic: read() called...\n");
+	debug_printk("read() called...");
 
 	if (lbuf > 2)
 		return -EMSGSIZE;
@@ -265,7 +289,10 @@ spi_protocol_generic_write(struct file *file_p, const char __user *buf,
 	u8 *write_buffer = kcalloc(2, sizeof(u8), GFP_KERNEL);
 	struct spi_transfer write_arduino[1] = {};
 
-	printk(KERN_DEBUG "spi-protocol-generic: write() %d bytes called...\n",
+	struct meta_information *meta_info = file_p->private_data;
+	struct spi_device *__spi_device_internal = meta_info->device;
+
+	debug_printk("write() %d bytes called...",
 	       lbuf);
 
 	if (lbuf > 2)
@@ -275,7 +302,7 @@ spi_protocol_generic_write(struct file *file_p, const char __user *buf,
 	if (err != 0)
 		return -EFAULT;
 
-	printk(KERN_WARNING "spi-protocol-generic: copy from us to ks ok\n");
+	printk(KERN_WARNING "spi-protocol-generic: copy from us to ks ok");
 
 	write_arduino[0].tx_buf = write_buffer;
 	write_arduino[0].rx_buf = NULL;
@@ -294,29 +321,37 @@ spi_protocol_generic_ioctl(struct file *file_p, unsigned int cmd,
 {
 	int retval = 0;
 	int status = 0;
+
 	struct register_info tmp;
 	struct spi_transfer *spi_xfers;
 	struct spi_message *spi_msg;
 
+	struct meta_information *meta_info = file_p->private_data;
+	struct spi_device *__spi_device_internal = meta_info->device;
+
 	switch (cmd) {
 	case SPI_GENERIC_SET_STATUS:
 		retval = copy_from_user(&tmp,
-					 (struct register_info __user *)arg,
-					 sizeof(struct register_info));
+					(struct register_info __user *)arg,
+					sizeof(struct register_info));
+
 		if (retval == 0) {
-			spi_xfers = __make_reg_transfers(&tmp);
+			spi_xfers = __make_set_reg_transfers(meta_info, &tmp);
 			if (IS_ERR(spi_xfers)) {
+				debug_printk("error in making spi_xfers!");
 				retval = PTR_ERR(spi_xfers);
 				break;
 			}
 
 			spi_msg = __make_reg_message(spi_xfers);
 			if (IS_ERR(spi_msg)) {
+				debug_printk("error in making spi_msg!");
 				retval = PTR_ERR(spi_msg);
 				break;
 			}
 
 			status = spi_sync(__spi_device_internal, spi_msg);
+
 			kfree(spi_msg);
 			kfree(spi_xfers);
 
@@ -328,27 +363,39 @@ spi_protocol_generic_ioctl(struct file *file_p, unsigned int cmd,
 		break;
 	case SPI_GENERIC_GET_STATUS:
 		retval = copy_from_user(&tmp,
-					 (struct register_info __user *)arg,
-					 sizeof(struct register_info));
+					(struct register_info __user *)arg,
+					sizeof(struct register_info));
 
 		if (retval == 0) {
-			spi_xfers = __make_get_reg_transfers(&tmp);
+			spi_xfers = __make_get_reg_transfers(meta_info, &tmp);
 			if (IS_ERR(spi_xfers)) {
+				debug_printk("error while making reg xfer");
 				retval = PTR_ERR(spi_xfers);
 				break;
 			}
 
 			spi_msg = __make_reg_message(spi_xfers);
 			if (IS_ERR(spi_msg)) {
+				debug_printk("error while making reg msg");
 				retval = PTR_ERR(spi_msg);
 				break;
 			}
 
 			status = spi_sync(__spi_device_internal, spi_msg);
+			//struct spi_transfer xfer;
+			//xfer.tx_buf = spi_xfers->tx_buf;
+			//debug_printk("buf %x",
+			//	     *((u8 *)(xfer.tx_buf)));
+			//xfer.speed_hz = 100000;
+			//spi_sync_transfer(__spi_device_internal, &xfer, 1);
+			debug_printk("answ byte %x",
+				     *((u8 *)((spi_xfers + 3)->rx_buf)));
 			kfree(spi_msg);
 			kfree(spi_xfers);
 
 			if (status == 0) {
+				debug_printk("from deviee: reg/val: %x/%x",
+					     tmp.reg_addr, tmp.value);
 				retval = copy_to_user(
 					(struct register_info __user *)arg,
 					&tmp,
@@ -359,7 +406,6 @@ spi_protocol_generic_ioctl(struct file *file_p, unsigned int cmd,
 		}
 		break;
 	}
-
 	return retval;
 }
 
@@ -376,66 +422,31 @@ static const struct file_operations spi_protocol_generic_fops = {
 static int spi_protocol_generic_probe(struct spi_device *spi)
 {
 	struct meta_information *device_meta_info;
-	int err, ret;
-	const struct of_device_id *match;
+	int err;
 
 	dev_t new_device_dev_t;
 
-	struct device_node *of_arduino_node;
-	u32 max_speed_arduino = 0;
+	struct device *sysfs_dev;
 
-	device_meta_info = kzalloc(sizeof(meta_information), GFP_KERNEL);
+	device_meta_info = kzalloc(sizeof(struct meta_information), GFP_KERNEL);
 	if (!device_meta_info)
 		return -ENOMEM;
 
 	device_meta_info->device = spi;
-	device_meta_info->default_values.speed_hz = spi->max_speed_hz;
-	device_meta_info->default_values.mode = spi->mode;
-	device_meta_info->default_values.bits_per_word = spi->bits_per_word;
-
-	// Initiate buffer for registry operations
 	device_meta_info->tx_buffer_reg = kzalloc(buf_reg_size, GFP_KERNEL);
 	device_meta_info->rx_buffer_reg = kzalloc(buf_reg_size, GFP_KERNEL);
 
-
-	debug_printk("spi-protocol-generic: default speed is %d Hz\n",
-		     default_values.speed_hz);
-
-	// check and read data from of_device_id...
-	match = of_match_device(spi_protocol_generic_of_match, &spi->dev);
-	if (!match) {
-		debug_printk("device not found in device tree...\n");
-		return -1;
-	}
-
-	// Here we can use of_get_property, but I prefer more readable
-	// code instead of more obvious.
-	// TODO: getting info from directly DT
-	of_arduino_node = spi->dev.of_node;
-	ret = of_property_read_u32(of_arduino_node, "spi-max-frequency",
-				   &max_speed_arduino);
-	if (ret) {
-		debug_printk("cannot find property in DT node, status %d\n",
-			     ret);
-		return ret;
-	} else {
-		debug_printk("freq is %x Hz\n", max_speed_arduino);
-	}
-
 	spi->bits_per_word = 8;
 	spi->mode = SPI_MODE_0;
-	spi->max_speed_hz = max_speed_arduino;
 	err = spi_setup(spi);
 
 	if (err < 0) {
-		printk(KERN_DEBUG "spi-protocol-generic: spi_setup failed!\n");
+		debug_printk("spi_setup failed!");
+		kfree(device_meta_info);
 		return err;
-	} else {
-		printk(KERN_DEBUG "spi-protocol-generic: now speed is %d Hz\n",
-		       spi->max_speed_hz);
 	}
 
-	printk(KERN_DEBUG "spi-protocol-generic: spi_setup ok, cs: %d\n",
+	debug_printk("spi_setup ok, cs: %d",
 	       spi->chip_select);
 
 	// BOO! Dark code...
@@ -452,36 +463,43 @@ static int spi_protocol_generic_probe(struct spi_device *spi)
 		err = register_chrdev_region(new_device_dev_t, 1,
 					     SPI_PROTOCOL_GENERIC_DEVICE);
 	}
-	device_meta_info->device_maj_min = new_device_dev_t;
 
 	if (err < 0) {
-		debug_printk("(alloc/register)_chrdev_region failed!\n");
+		kfree(device_meta_info);
 		return err;
 	}
 
-	spi_protocol_generic_cdev = cdev_alloc();
-	if (!(spi_protocol_generic_cdev)) {
-		printk(KERN_DEBUG "spi_protocol_generic: cdev_alloc failed!\n");
+	device_meta_info->internal_cdev = cdev_alloc();
+	if (device_meta_info->internal_cdev == NULL) {
+		kfree(device_meta_info);
 		unregister_chrdev_region(first_dev_t, 1);
-		return -1;
+		return -ENOMEM;
 	}
 
-	cdev_init(spi_protocol_generic_cdev, &spi_protocol_generic_fops);
-	err = cdev_add(spi_protocol_generic_cdev, new_device_dev_t, 1);
+	cdev_init(device_meta_info->internal_cdev, &spi_protocol_generic_fops);
+	err = cdev_add(device_meta_info->internal_cdev, new_device_dev_t, 1);
 	if(err < 0) {
-		printk(KERN_DEBUG "spi_protocol_generic: cdev_add failed!\n");
-		cdev_del(spi_protocol_generic_cdev);
+		kfree(device_meta_info);
 		unregister_chrdev_region(new_device_dev_t, 1);
+		cdev_del(device_meta_info->internal_cdev);
 		return err;
 	}
 
-	spi_protocol_generic_dev =
-		device_create(spi_protocol_generic_class, NULL,
-			      new_device_dev_t, NULL, "%s%d",
-			      SPI_PROTOCOL_GENERIC_DEVICE, minorcount - 1);
+	sysfs_dev = device_create(spi_protocol_generic_class, NULL,
+				  new_device_dev_t, device_meta_info,
+				  "%s.%d",
+				  SPI_PROTOCOL_GENERIC_DEVICE,
+				  MINOR(new_device_dev_t));
+	if (IS_ERR(sysfs_dev)) {
+		kfree(device_meta_info);
+		unregister_chrdev_region(new_device_dev_t, 1);
+		cdev_del(device_meta_info->internal_cdev);
+		return PTR_ERR(sysfs_dev);
+	}
 
 	//
 
+	device_meta_info->device_maj_min = new_device_dev_t;
 	list_add(&device_meta_info->list_entry, &meta_info_list);
 	spi_set_drvdata(spi, device_meta_info);
 
@@ -492,26 +510,21 @@ static int spi_protocol_generic_remove(struct spi_device *spi)
 {
 	struct meta_information *device_meta_info = spi_get_drvdata(spi);
 
-	printk(KERN_DEBUG "spi-protocol-generic: remove().\n");
+	debug_printk("remove().");
 
 	list_del(&device_meta_info->list_entry);
-	device_destroy(spi_protocol_generic_class, first_dev_t + index);
+	device_destroy(spi_protocol_generic_class,
+		       device_meta_info->device_maj_min);
 
 	kfree(device_meta_info->tx_buffer_reg);
 	kfree(device_meta_info->rx_buffer_reg);
 
-	spi->max_speed_hz = default_values.speed_hz;
-	spi->mode = default_values.mode;
-	spi->bits_per_word = default_values.bits_per_word;
-	spi_setup(spi);
-
 	// BOO! Dark code...
 	// TODO: LOOK THROUGH IT!
 
-	if(spi_protocol_generic_cdev) {
-		cdev_del(spi_protocol_generic_cdev);
-	}
-	unregister_chrdev_region(first_dev_t + index, 1);
+	cdev_del(device_meta_info->internal_cdev);
+	unregister_chrdev_region(device_meta_info->device_maj_min, 1);
+	kfree(device_meta_info);
 
 	//
 	return 0;
@@ -533,25 +546,25 @@ static int __init spi_protocol_generic_init(void)
 
 	minorcount = 0;
 
-	status = spi_register_driver(&spi_protocol_generic);
-	if (status < 0)
-		return status;
-
 	// define a device class
 	spi_protocol_generic_class = class_create(THIS_MODULE, SPI_CLASS_NAME);
 	if (IS_ERR(spi_protocol_generic_class)) {
-		printk(KERN_DEBUG "spi_protocol_generic: class_create failed!\n");
+		debug_printk("class_create failed!");
 		spi_unregister_driver(&spi_protocol_generic);
 		return PTR_ERR(spi_protocol_generic_class);
 	}
 
+	status = spi_register_driver(&spi_protocol_generic);
+	if (status < 0)
+		return status;
+
 	return status;
 }
 
-static void __exit spi_protocol_generic_exit(void) {
-	class_destroy(spi_protocol_generic_class);
-
+static void __exit spi_protocol_generic_exit(void)
+{
 	spi_unregister_driver(&spi_protocol_generic);
+	class_destroy(spi_protocol_generic_class);
 }
 
 module_init(spi_protocol_generic_init);
